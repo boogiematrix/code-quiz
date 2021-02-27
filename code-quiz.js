860ffaf1838c73b593d2f34f150d1565eb70e4b4
Night Chough <chicken.snuggles@gmail.com>
2021-02-27 09:27:22 -0600

    laid out html

---
+++ code-quiz.js
@@ -0,0 +1,24 @@
+/*GIVEN I am taking a code quiz
+WHEN I click the start button
+todo: link a button event to the start of timer and quiz
+THEN a timer starts and I am presented with a question
+todo: create question and answer format and library
+WHEN I answer a question
+todo: use radial format for multiple choice
+THEN I am presented with another question
+todo: link button event to refresh question area
+WHEN I answer a question incorrectly
+todo: id correct and incorrect responses
+THEN time is subtracted from the clock
+todo: if/else that displays correct or incorrect, and subtracts time if
+incorrect, and adds points if correct
+WHEN all questions are answered or the timer reaches 0
+todo: stop timer at 0
+THEN the game is over
+todo: make game over screen
+WHEN the game is over
+THEN I can save my initials and my score
+create a function that appends a list with an 'initials' input along with
+the user's score
+*/
+
